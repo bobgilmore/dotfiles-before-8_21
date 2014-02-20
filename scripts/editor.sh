535d8f73bff68c52c691e9c3bf26df33d36d1289
Bob Gilmore <dev@bobgilmore.name>
2014-02-20 19:50:55 +0000

    Make editor script work on Linux for all file types.

--- scripts/editor.sh
+++ scripts/editor.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
- 
-case "$1" in
+
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+    case "$1" in
 	*_EDITMSG|*MERGE_MSG|*_TAGMSG )
-		/usr/local/bin/vim "$1"
-		;;
+	    emacs "$1"
+	    ;;
 	*.md )
-		$HOME/bin/mmdc "$1"
-		;;
+	    $HOME/bin/mmdc "$1"
+	    ;;
 	*.txt )
-		$HOME/bin/mmdc "$1"
-		;;
+	    $HOME/bin/mmdc "$1"
+	    ;;
 	* )
-		$HOME/bin/subl -w "$1"
-		;;
-esac
+	    $HOME/bin/subl -w "$1"
+	    ;;
+    esac
+else
+    emacs "$1";
+fi
