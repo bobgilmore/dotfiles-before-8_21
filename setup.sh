7e234904afca9e3737c3c965ca3992cedf813af3
Bob Gilmore <dev@bobgilmore.name>
2014-03-21 00:23:05 -0400

    Add KeyRemap4MacBook's private.xml file into the mix

--- setup.sh
+++ setup.sh
@@ -207,7 +207,21 @@ then
       echo "Can't directory $CRIT_DIR for symlinks to the CriticMarkup-toolkit scripts."
     fi
   fi
-  
+
+  echo "------ TODO ------"
+  if [ -d "/Applications/KeyRemap4MacBook.app/" ]
+  then
+    mkdir -m 755 -p "$HOME/Library/Application Support/KeyRemap4MacBook/"
+    link_if_necessary "$DOTFILE_DIRECTORY" "$HOME/Library/Application Support/KeyRemap4MacBook" "private.xml"
+    /Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli reloadxml
+    echo "When installation is complete,"
+    echo "Run KeyRemap4MacBook, open its Preference pane, go to the Change Key panel, and check all of the check boxes at the top of the remapping list."
+    echo "This will enable Hyper key.  See http://brettterpstra.com/2012/12/08/a-useful-caps-lock-key/ for more on implementing it." 
+  else
+    echo "You should install KeyRemap4MacBook.  It's awesome!  Then run this setup script again."
+  fi
+  echo "---- END TODO ----"
+
   echo "Running .osx preferences script"
   chmod 777 ./.osx
   ./.osx
