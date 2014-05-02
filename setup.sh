98bdddc6c0bb8a8dd3656c484c3c932e2b97022a
Bob Gilmore <dev@bobgilmore.name>
2014-05-02 19:59:19 -0400

    Break env vars and functions out into own script

--- setup.sh
+++ setup.sh
@@ -1,37 +1,7 @@
 #!/bin/bash
 
-# Find the location of the script.
-DOTFILE_DIRECTORY=`pwd`
-BIN_DIRECTORY="$HOME/bin"
-SCRIPT_DIRECTORY="$HOME/scripts"
-DROPBOX_DIRECTORY="$HOME/Dropbox/dotfile_symlinks"
-LIB_SUBLIME_2="Library/Application Support/Sublime Text 2"
-LIB_SUBLIME_3="Library/Application Support/Sublime Text 3"
-HOME_VIM="$HOME/.vim"
-
-# Three-argument form: sourcedir, targetdir, source_and_target_file
-# Four-argument form:  sourcedir, targetdir, sourcefile, targetfile
-link_if_necessary() {
-  local sourcefile="$3"
-  local sourcedir="$1"
-  local targetdir="$2"
-  if [ -z "$4" ]
-  then
-    local targetfile="$3"
-  else
-    local targetfile="$4"
-  fi
-
-  if [ ! -e "$targetdir/$targetfile" ]; then
-    ln -is "$sourcedir/$sourcefile" "$targetdir/$targetfile"
-  else
-    if [[ $(readlink "$targetdir/$targetfile") != "$sourcedir/$sourcefile" ]]; then
-      local full_targ="$targetdir/$targetfile"
-      rm -rf "${full_targ%/}"
-      ln -is "$sourcedir/$sourcefile" "$full_targ"
-    fi
-  fi
-}
+# Load env vars and functions
+source "$(pwd)/setup_utils.sh"
 
 if [ $(uname) = 'Darwin' ]; then
   if [ ! -f /usr/local/bin/brew ]; then
---
+++ setup_utils.sh
@@ -0,0 +1,34 @@
+# Define env vars and functions required by setup scripts
+
+# Find the location of the script.
+DOTFILE_DIRECTORY=`pwd`
+BIN_DIRECTORY="$HOME/bin"
+SCRIPT_DIRECTORY="$HOME/scripts"
+DROPBOX_DIRECTORY="$HOME/Dropbox/dotfile_symlinks"
+LIB_SUBLIME_2="Library/Application Support/Sublime Text 2"
+LIB_SUBLIME_3="Library/Application Support/Sublime Text 3"
+HOME_VIM="$HOME/.vim"
+
+# Three-argument form: sourcedir, targetdir, source_and_target_file
+# Four-argument form:  sourcedir, targetdir, sourcefile, targetfile
+link_if_necessary() {
+  local sourcefile="$3"
+  local sourcedir="$1"
+  local targetdir="$2"
+  if [ -z "$4" ]
+  then
+    local targetfile="$3"
+  else
+    local targetfile="$4"
+  fi
+
+  if [ ! -e "$targetdir/$targetfile" ]; then
+    ln -is "$sourcedir/$sourcefile" "$targetdir/$targetfile"
+  else
+    if [[ $(readlink "$targetdir/$targetfile") != "$sourcedir/$sourcefile" ]]; then
+      local full_targ="$targetdir/$targetfile"
+      rm -rf "${full_targ%/}"
+      ln -is "$sourcedir/$sourcefile" "$full_targ"
+    fi
+  fi
+}
