fac5fd7017202b5e29ea663cf776b51303dbde32
Bob Gilmore <dev@bobgilmore.name>
2014-05-02 20:46:36 -0400

    Break Mac app setup out into a separate script

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ source "$(pwd)/setup_utils.sh"
 ./sublime_setup.sh
 ./git_setup.sh
 ./vim_setup.sh
+./mac_app_setup.sh
 
 # CriticMarkup no longer installed.
 #./criticmarkup_setup.sh
@@ -33,48 +34,10 @@ fi
 # Perform operations that only make sense on a Mac...
 if [ $(uname) = 'Darwin' ]
 then
-  
-  # Install Brett Terpstra's na script - https://github.com/ttscoff/na
-  if [ -d "/Applications/TaskPaper.app" ]
-  then
-    NA_DIR="$DOTFILE_DIRECTORY/na"
-    if [ -d "$NA_DIR" ]; then
-      echo "Updating Brett Terpstra's NA script for TaskPaper"
-      cd $NA_DIR && git pull
-    else
-      # Cloning to $NA_DIR
-      echo "Installing Brett Terpstra's NA script for TaskPaper; cloning NA repo into $NA_DIR"
-      git clone https://github.com/ttscoff/na.git $NA_DIR
-    fi
-    cd $DOTFILE_DIRECTORY
-    if [ -d "$NA_DIR" ]
-    then
-      echo "Symlinking NA in $SCRIPT_DIRECTORY"
-      link_if_necessary "$NA_DIR" "$SCRIPT_DIRECTORY" "na.sh"
-    else
-      echo "Can't find directory $NA_DIR for symlink to the NA script."
-    fi
-  else
-    echo "Skipping NA since TaskPaper not installed."
-  fi
 
   echo "Configuring bundle..."
   number_of_cores=$(sysctl -n hw.ncpu)
   bundle config --global jobs $((number_of_cores - 1))
-
-  echo "------ TODO ------"
-  if [ -d "/Applications/KeyRemap4MacBook.app/" ]
-  then
-    mkdir -m 755 -p "$HOME/Library/Application Support/KeyRemap4MacBook/"
-    link_if_necessary "$DOTFILE_DIRECTORY" "$HOME/Library/Application Support/KeyRemap4MacBook" "private.xml"
-    /Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli reloadxml
-    echo "When installation is complete,"
-    echo "Run KeyRemap4MacBook, open its Preference pane, go to the Change Key panel, and check all of the check boxes at the top of the remapping list."
-    echo "This will enable Hyper key.  See http://brettterpstra.com/2012/12/08/a-useful-caps-lock-key/ for more on implementing it." 
-  else
-    echo "You should install KeyRemap4MacBook.  It's awesome!  Then run this setup script again."
-  fi
-  echo "---- END TODO ----"
 
   echo "Running .osx preferences script"
   chmod 777 ./.osx
