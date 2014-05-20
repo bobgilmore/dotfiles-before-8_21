253e1ea4287f25c732cbfdce23ca4f28a477f329
Bob Gilmore <dev@bobgilmore.name>
2014-05-20 15:00:28 -0400

    Install NA script even if TaskPaper isn't present

--- setup_scripts/mac_app_setup.sh
+++ setup_scripts/mac_app_setup.sh
@@ -10,26 +10,29 @@ if [ $(uname) = 'Darwin' ]
 then
   
   # Install Brett Terpstra's na script - https://github.com/ttscoff/na
-  if [ -d "/Applications/TaskPaper.app" ]
-  then
-    NA_DIR="$DOTFILE_DIRECTORY/na"
-    if [ -d "$NA_DIR" ]; then
-      echo "Updating NA script for TaskPaper.  See http://brettterpstra.com/projects/na/"
-      cd $NA_DIR && git pull
-    else
-      # Cloning to $NA_DIR
-      echo "Installing NA script for TaskPaper into $NA_DIR.  See http://brettterpstra.com/projects/na/"
-      git clone https://github.com/ttscoff/na.git $NA_DIR
-    fi
-    cd $DOTFILE_DIRECTORY
-    if [ -d "$NA_DIR" ]
-    then
-      link_if_necessary "$NA_DIR" "$ALIASES_DIRECTORY" "na.sh"
-    else
-      echo "Error: $NA_DIR not found."
-    fi
+  
+  NA_DIR="$DOTFILE_DIRECTORY/na"
+  if [ -d "$NA_DIR" ]; then
+    echo "Updating NA script for TaskPaper.  See http://brettterpstra.com/projects/na/"
+    cd $NA_DIR && git pull
   else
-    echo "Skipping NA since TaskPaper not installed."
+    # Cloning to $NA_DIR
+    echo "Installing NA script for TaskPaper into $NA_DIR.  See http://brettterpstra.com/projects/na/"
+    git clone https://github.com/ttscoff/na.git $NA_DIR
+  fi
+  cd $DOTFILE_DIRECTORY
+  if [ -d "$NA_DIR" ]
+  then
+    link_if_necessary "$NA_DIR" "$ALIASES_DIRECTORY" "na.sh"
+  else
+    echo "Error: $NA_DIR not found."
+  fi
+  
+  if [ ! -d "/Applications/TaskPaper.app" ]
+  then
+    echo "------ TODO ------"
+    echo "You should install TaskPaper - it's great."
+    echo "---- END TODO ----"
   fi
 
   echo "------ TODO ------"
