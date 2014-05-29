dc5aca0afcbcd373cc5fbdcf85c649f9b996b7c3
Bob Gilmore <dev@bobgilmore.name>
2014-05-28 22:08:11 -0400

    Add examples for DASH and DMAN when no arguments given

--- aliases/dash.bash
+++ aliases/dash.bash
@@ -1,5 +1,7 @@
 # From http://brettterpstra.com/2014/05/10/bash-and-dash/
 # url encode the passed string
+# Example:
+# dash ruby:FileUtils cp_r
 urlenc () {
   echo -n "$@" | perl -pe's/([^-_.~A-Za-z0-9])/sprintf("%%%02X", ord($    1))/seg'
 }
@@ -8,10 +10,24 @@ urlenc () {
 # Example:
 # dash ruby:FileUtils cp_r
 function dash() {
-  open "dash://$(urlenc $@)"
+  if [[ $# -eq 0 ]] ; then
+    echo 'DASH'
+    echo 'Opens documentation in Dash.'
+    echo 'Example:'
+    echo 'dash ruby:FileUtils cp_r'
+  else
+    open "dash://$(urlenc $@)"
+  fi
 }
 
 function dman() {
-  open "dash://man:$(urlenc $@)"
+  if [[ $# -eq 0 ]] ; then
+    echo 'DMAN'
+    echo 'Opens MAN pages in DASH.'
+    echo 'Example:'
+    echo 'dman sed'
+  else
+    open "dash://man:$(urlenc $@)"
+  fi
 }
 
