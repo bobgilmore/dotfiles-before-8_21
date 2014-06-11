1eabf789edbf1fbb8908005fe923aac5f28adf98
Bob Gilmore <dev@bobgilmore.name>
2014-06-10 22:12:33 -0400

    Use vim as the default editor

--- scripts/editor.sh
+++ scripts/editor.sh
@@ -3,17 +3,11 @@
 unamestr=`uname`
 if [[ "$unamestr" == 'Darwin' ]]; then
     case "$1" in
-	*_EDITMSG|*MERGE_MSG|*_TAGMSG )
-	    $HOME/bin/subl -w "$1"
+	  *.md )
+	    mmdc "$1"
 	    ;;
-	*.md )
-	    $HOME/bin/mmdc "$1"
-	    ;;
-	*.txt )
-	    $HOME/bin/mmdc "$1"
-	    ;;
-	* )
-	    $HOME/bin/subl -w "$1"
+	  * )
+      vim "$1"
 	    ;;
     esac
 else
