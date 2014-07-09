ba3222d694043e726ea2c78ea2fe27a0e01db7d6
Bob Gilmore <dev@bobgilmore.name>
2014-07-09 11:18:34 -0400

    Symlink powerline config files to correct location

--- setup_scripts/dotfile_setup.sh
+++ setup_scripts/dotfile_setup.sh
@@ -8,7 +8,17 @@ source "$(pwd)/setup_scripts/setup_utils.sh"
 echo "Symlinking dotfiles in ~"
 for f in $DOTFILE_DIRECTORY/.* ; do
   base=$(basename $f)
-  if [[ ($base != ".") && ($base != "..") && ($base != ".git") ]]; then
+  if [[ ($base != ".") && ($base != "..") && ($base != ".git") && ($base != ".config") ]]; then
     link_if_necessary "$DOTFILE_DIRECTORY" "$HOME" $base
   fi
 done
+
+echo "Symlinking .config in ~"
+mkdir -p "$HOME/.config"
+for f in $DOTFILE_DIRECTORY/.config/* ; do
+  base=$(basename $f)
+  if [[ ($base != ".") && ($base != "..") ]]; then
+    link_if_necessary "$DOTFILE_DIRECTORY/.config" "$HOME/.config" $base
+  fi
+done
+
