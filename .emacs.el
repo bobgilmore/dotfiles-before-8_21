9838374bc3b8e9a575829e2db76d5ec21866a49f
Bob Gilmore <dev@bobgilmore.name>
2015-01-05 10:14:17 -0500

    Change Emacs backup location

--- .emacs.el
+++ .emacs.el
@@ -1,1 +1,9 @@
 (global-set-key (kbd "C-c C-v") 'shell)
+
+;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
+(custom-set-variables
+ '(auto-save-file-name-transforms '((".*" "~/.emacs_backups/autosaves/\\1" t)))
+ '(backup-directory-alist '((".*" . "~/.emacs_backups/backups/"))))
+
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs_backups/autosaves/" t)
