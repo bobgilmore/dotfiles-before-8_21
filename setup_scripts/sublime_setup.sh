ae196059b2e6649ee53fe91d2b519bb8d7abb54a
Bob Gilmore <dev@bobgilmore.name>
2015-02-27 13:28:59 -0500

    Fix Sublime setup script and make it v. 3 only

--- setup_scripts/sublime_setup.sh
+++ setup_scripts/sublime_setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Set up Sublime Text - version 2 and 3
+# Set up Sublime Text 3
 
 # Load env vars and functions
 source "$(pwd)/setup_scripts/setup_utils.sh"
 
-echo "Sublime Text..."
 if [ $(uname) = 'Darwin' ]; then
+  echo "Sublime Text..."
+
   # Create the "subl" symlink to open Sublime Text (3) from the cli
   if [ -f "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ]
   then
@@ -18,31 +19,23 @@ if [ $(uname) = 'Darwin' ]; then
 
   # Sublime Text preferences etc. to be shared between multiple machines.
 
-  # These files are constantly-changing and shouldn't be checked in.
+  # These files are constantly changing and shouldn't be checked in.
   # Store them in Dropbox, and make all machines point to the Dropbox copies.
-
-  # Handle Sublime 2 Preferences and packages.
-  if [ ! -d "$DROPBOX_DIRECTORY/$LIB_SUBLIME_2/Installed Packages" ]
-  then
-    mkdir -p "$DROPBOX_DIRECTORY/$LIB_SUBLIME_2/Installed Packages"
-    mkdir -p "$DROPBOX_DIRECTORY/$LIB_SUBLIME_2/Packages"
-    mkdir -p "$DROPBOX_DIRECTORY/$LIB_SUBLIME_2/Pristine Packages"
-  fi
-  echo "Symlinking Sublime Text 2 in ~/Library/Application Support/"
-  link_if_necessary "$DROPBOX_DIRECTORY/$LIB_SUBLIME_2" "$HOME/$LIB_SUBLIME_2" "Installed Packages"
-  link_if_necessary "$DROPBOX_DIRECTORY/$LIB_SUBLIME_2" "$HOME/$LIB_SUBLIME_2" "Packages"
-  link_if_necessary "$DROPBOX_DIRECTORY/$LIB_SUBLIME_2" "$HOME/$LIB_SUBLIME_2" "Pristine Packages"
-
-  # Handle Sublime 3 Preferences and packages.
-  # Many fewer files are shared in Sublime 3 since it, itself, loads new packages as necessary.
   # See https://sublime.wbond.net/docs/syncing#dropbox-osx
-  if [ ! -d "$DROPBOX_DIRECTORY/$LIB_SUBLIME_3/Packages/User" ]
+  if [ ! -d "$DROPBOX_DIRECTORY/$LIB_SUBLIME_3/Packages" ]
   then
-    mkdir -p "$DROPBOX_DIRECTORY/$LIB_SUBLIME_3/Packages/User"
+    mkdir -p "$DROPBOX_DIRECTORY/$LIB_SUBLIME_3/Packages"
+  fi
+  if [ ! -d "$HOME/$LIB_SUBLIME_3" ]
+  then
+    mkdir -p "$HOME/$LIB_SUBLIME_3"
   fi
   echo "Symlinking Sublime Text 3 in ~/Library/Application Support/"
-  link_if_necessary "$DROPBOX_DIRECTORY/$LIB_SUBLIME_3/Packages" "$HOME/$LIB_SUBLIME_3/Packages" "User"
+  link_if_necessary "$DROPBOX_DIRECTORY/$LIB_SUBLIME_3" "$HOME/$LIB_SUBLIME_3" "Packages"
+  link_if_necessary "$DROPBOX_DIRECTORY/$LIB_SUBLIME_3" "$HOME/$LIB_SUBLIME_3" "Installed Packages"
+  link_if_necessary "$DOTFILE_DIRECTORY" "$HOME/$LIB_SUBLIME_3/Packages/User" "Preferences.sublime-settings"
+
+  echo "...Sublime Text done."
 else
-  echo "Skipping Sublime Text setup on non-Mac. To fix this, edit sublime_setup.sh."
+  echo "Skipping Sublime Text setup on non-Mac. To silence this message, edit sublime_setup.sh."
 fi
-echo "...Sublime Text done."
