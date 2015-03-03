9d8f63425137ae9a40422fa18e7bca8d714d8d7a
Bob Gilmore <dev@bobgilmore.name>
2015-03-03 13:47:08 -0500

    React to KeyRemap4MacBook -> Karabiner name change

--- setup_scripts/mac_app_setup.sh
+++ setup_scripts/mac_app_setup.sh
@@ -31,21 +31,21 @@ then
   if [ ! -d "/Applications/TaskPaper.app" ]
   then
     echo "------ TODO ------"
-    echo "You should install TaskPaper - it's great."
+    echo "You should install TaskPaper."
     echo "---- END TODO ----"
   fi
 
   echo "------ TODO ------"
-  if [ -d "/Applications/KeyRemap4MacBook.app/" ]
+  if [ -d "/Applications/Karabiner.app/" ]
   then
-    mkdir -m 755 -p "$HOME/Library/Application Support/KeyRemap4MacBook/"
-    link_if_necessary "$DOTFILE_DIRECTORY" "$HOME/Library/Application Support/KeyRemap4MacBook" "private.xml"
-    /Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli reloadxml
+    mkdir -m 755 -p "$HOME/Library/Application Support/Karabiner/"
+    link_if_necessary "$DOTFILE_DIRECTORY" "$HOME/Library/Application Support/Karabiner" "private.xml"
+    /Applications/Karabiner.app/Contents/Library/bin/karabiner reloadxml
     echo "When installation is complete,"
-    echo "Run KeyRemap4MacBook, open its Preference pane, go to the Change Key panel, and check all of the check boxes at the top of the remapping list."
+    echo "Run Karabiner, open its Preference pane, go to the Change Key panel, and check all of the check boxes at the top of the remapping list."
     echo "This will enable Hyper key.  See http://brettterpstra.com/2012/12/08/a-useful-caps-lock-key/ for more on implementing it." 
   else
-    echo "You should install KeyRemap4MacBook - it's awesome!  Then run this setup script again."
+    echo "You should install Karabiner, then run this setup script again."
   fi
   echo "---- END TODO ----"
 fi
