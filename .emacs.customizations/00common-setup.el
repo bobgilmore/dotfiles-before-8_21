fa7c51ba39ade964a5568209a2e5a5ad4bc29ca7
Bob Gilmore <dev@bobgilmore.name>
2015-03-09 22:55:48 -0400

    Move some of .emacs.el out into separate files

---
+++ .emacs.customizations/00common-setup.el
@@ -0,0 +1,37 @@
+(global-set-key (kbd "C-c C-v") 'shell)
+
+;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
+(custom-set-variables
+ '(auto-save-file-name-transforms '((".*" "~/.emacs_backups/autosaves/\\1" t)))
+ '(backup-directory-alist '((".*" . "~/.emacs_backups/backups/")))
+ '(frame-background-mode 'dark)
+ )
+
+(load-theme 'solarized t)
+(load-theme 'tangotango t)
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs_backups/autosaves/" t)
+
+(require 'highlight-chars)
+(add-hook 'font-lock-mode-hook 'hc-highlight-tabs)
+
+(require 'ws-trim)
+(global-ws-trim-mode t)
+(set-default 'ws-trim-level 1)
+
+;; http://www.emacswiki.org/emacs/DotEmacsChallenge
+(setq-default indent-tabs-mode nil)
+(setq require-final-newline 't)
+(add-hook 'comint-output-filter-functions 'comint-watch-for-password-prompt)
+(global-set-key "\C-cg" 'goto-line)
+(global-set-key "\C-cG" 'goto-char)
+(global-set-key "\C-cw" 'delete-region) ; ala C-w and M-C-w
+(global-set-key "\C-cc" 'comment-region)
+(global-set-key "\C-cu" 'uncomment-region)
+
+(column-number-mode 1)
+
+;; From https://gist.github.com/gnufied/7160310
+(prefer-coding-system 'utf-8)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
--- .emacs.el
+++ .emacs.el
@@ -1,37 +1,10 @@
-(global-set-key (kbd "C-c C-v") 'shell)
-
-;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
-(custom-set-variables
- '(auto-save-file-name-transforms '((".*" "~/.emacs_backups/autosaves/\\1" t)))
- '(backup-directory-alist '((".*" . "~/.emacs_backups/backups/"))))
-
-;; create the autosave dir if necessary, since emacs won't.
-(make-directory "~/.emacs_backups/autosaves/" t)
-
 (require 'cask "/usr/local/Cellar/cask/0.7.2/cask.el")
 (cask-initialize)
 (require 'pallet)
 
-(require 'highlight-chars)
-(add-hook 'font-lock-mode-hook 'hc-highlight-tabs)
-
-(require 'ws-trim)
-(global-ws-trim-mode t)
-(set-default 'ws-trim-level 1)
-
-;; http://www.emacswiki.org/emacs/DotEmacsChallenge
-(setq-default indent-tabs-mode nil)
-(setq require-final-newline 't)
-(add-hook 'comint-output-filter-functions 'comint-watch-for-password-prompt)
-(global-set-key "\C-cg" 'goto-line)
-(global-set-key "\C-cG" 'goto-char)
-(global-set-key "\C-cw" 'delete-region) ; ala C-w and M-C-w
-(global-set-key "\C-cc" 'comment-region)
-(global-set-key "\C-cu" 'uncomment-region)
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/color-theme-tangotango")
 
-(column-number-mode 1)
-(mapc 'load (directory-files "~/.emacs.d/customizations" t "^[0-9]+.*\.el$"))
+(mapc 'load (directory-files "~/.emacs.customizations" t "^[0-9]+.*\.el$"))
