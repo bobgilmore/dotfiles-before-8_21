364d4d48706a030c9b6858d243a14bd1dee9f505
Bob Gilmore <dev@bobgilmore.name>
2015-03-28 01:00:45 -0400

    Update npm setup

--- setup_scripts/npm_setup.sh
+++ setup_scripts/npm_setup.sh
@@ -7,6 +7,8 @@ source "$(pwd)/setup_scripts/setup_utils.sh"
 
 echo "npm..."
 if [ $(uname) = 'Darwin' ]; then
+  npm install npm -g
+  npm update -g
   npm install -g bower
   npm install -g ember-cli
   npm install -g phantomjs
