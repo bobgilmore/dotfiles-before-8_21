3c566761dd94e2cfc50334a451ad78ed95298bf6
Bob Gilmore <dev@bobgilmore.name>
2015-04-16 08:31:06 -0400

    Delete git hook setup
    This code has moved into the git hooks repo itself, in order to break
    the coupling between the two repos.

--- setup_scripts/git_setup.sh
+++ setup_scripts/git_setup.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-# Set up git configuration
-
-# Load env vars and functions
-source "$(pwd)/setup_scripts/setup_utils.sh"
-
-echo "Git..."
-echo "Symlinking git hook directory to ~/.git_hook_symlink_installer"
-link_if_necessary "$DOTFILE_DIRECTORY" "$HOME" ".git_hook_symlink_installer"
-
-# If writing to init.templatedir would overwrite an existing value,
-# cache the old value away.
-gitconfig_init_templatedir=`git config --global --get init.templatedir`
-gitconfig_init_templatedirOLD=`git config --global --get init.templatedirOLD`
-if [[ ("${#gitconfig_init_templatedir}" > 0) && ("${#gitconfig_init_templatedirOLD}" == 0) ]]; then
-  echo "Old value of git variable init.templatedir copied to init.templatedirOLD"
-  git config --global init.templatedirOLD "$gitconfig_init_templatedir"
-fi
-# OK, now, write (or overwrite) init.templatedir value.
-git config --global init.templatedir '~/.git_hook_symlink_installer'
+# Set up shared git configuration
 
 gitconfig_include_path=`git config --global --get-all include.path`
 if [[ ! $gitconfig_include_path == *~/.gitconfig_shared* ]] ; then
