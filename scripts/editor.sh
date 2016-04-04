192ddfb84e650992af8bfb20851671886aa23c26
Bob Gilmore <dev@bobgilmore.name>
2016-04-04 09:29:42 -0400

    Switch main code editor of choice to Atom

--- scripts/editor.sh
+++ scripts/editor.sh
@@ -4,7 +4,8 @@ unamestr=$(uname)
 if [[ "$unamestr" == 'Darwin' ]]; then
     case "$1" in
   *_EDITMSG|*MERGE_MSG|*_TAGMSG )
-      "$HOME/bin/subl -w $1"
+      #"$HOME/bin/subl -w $1"
+      atom --wait "$1";
       ;;
   *.md )
       "$HOME/bin/mmdc $1"
