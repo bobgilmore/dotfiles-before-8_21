1b43bbf32f7e911e61ae1012712d5f3812873339
Bob Gilmore <dev@bobgilmore.name>
2018-09-05 13:29:07 -0400

    Update cask.el version

--- .emacs.el
+++ .emacs.el
@@ -5,7 +5,7 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(require 'cask "/usr/local/Cellar/cask/0.8.3/cask.el")
+(require 'cask "/usr/local/Cellar/cask/0.8.4/cask.el")
 (cask-initialize)
 (require 'pallet)
 
