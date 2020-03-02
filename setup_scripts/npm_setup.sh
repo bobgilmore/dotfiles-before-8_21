7ea32ab8cbdc0376e4659bacd25dee8c0b76bc2e
Bob Gilmore <dev@bobgilmore.name>
2020-03-02 11:05:00 -0500

    Tweaks npm package installation
    Mostly removes Cordova-related installations

--- setup_scripts/npm_setup.sh
+++ setup_scripts/npm_setup.sh
@@ -10,13 +10,13 @@ if [ "$(uname)" = 'Darwin' ]; then
   npm install npm -g
   npm update -g
   npm install -g bower
-  npm install -g ember-cli
-  npm install -g phantomjs
+  #npm install -g ember-cli
+  #npm install -g phantomjs
   npm install -g six
   npm install six
 
-  npm install -g cordova
-  npm install -g ios-deploy
-  npm install -g ios-sim
+  #npm install -g cordova
+  #npm install -g ios-deploy
+  #npm install -g ios-sim
 fi
 echo "...npm done."
