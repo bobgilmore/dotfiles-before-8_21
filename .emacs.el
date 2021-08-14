e77efa98932317c321642b0ec6aeeff3bc421867
Bob Gilmore <dev@bobgilmore.name>
2021-08-14 16:57:03 -0400

    Various Emacs changes

--- .emacs.el
+++ .emacs.el
@@ -3,9 +3,9 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+;;(package-initialize)
 (setq max-specpdl-size 5000)
-(require 'cask "/usr/local/Cellar/cask/0.8.4/cask.el")
+(require 'cask "/usr/local/Cellar/cask/0.8.7/cask.el")
 (cask-initialize)
 (require 'pallet)
 
@@ -22,15 +22,13 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
- '(auto-save-file-name-transforms (quote ((".*" "~/.emacs_backups/autosaves/\\1" t))))
- '(backup-directory-alist (quote ((".*" . "~/.emacs_backups/backups/"))))
+ '(auto-save-file-name-transforms '((".*" "~/.emacs_backups/autosaves/\\1" t)))
+ '(backup-directory-alist '((".*" . "~/.emacs_backups/backups/")))
  '(custom-safe-themes
-   (quote
-    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" default)))
- '(frame-background-mode (quote dark))
+   '("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" default))
+ '(frame-background-mode 'dark)
  '(package-selected-packages
-   (quote
-    (drag-stuff exec-path-from-shell expand-region htmlize idle-highlight-mode magit multiple-cursors nyan-mode popwin prodigy req-package smartparens smex use-package web-mode yasnippet package-build shut-up epl git commander f dash s))))
+   '(drag-stuff exec-path-from-shell expand-region htmlize idle-highlight-mode magit multiple-cursors nyan-mode popwin prodigy req-package smartparens smex use-package web-mode yasnippet package-build shut-up epl git commander f dash s)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
